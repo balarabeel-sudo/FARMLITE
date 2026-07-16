5e12934e2629d853e0461b13751066c12a82158a
balarabeel-sudo <balarabeel@gmail.com>
2026-07-16 05:05:18 +0100

    Wrap HomePage components in MainLayout

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,14 +1,15 @@
+import MainLayout from "../components/MainLayout";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import QuickActions from "../components/QuickActions";
 
 function HomePage() {
   return (
-    <>
+    <MainLayout>
       <Navbar />
       <HeroSection />
       <QuickActions />
-    </>
+    </MainLayout>
   );
 }
 
