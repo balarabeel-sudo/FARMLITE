2a4b12d9ce64cf1b0a9a7b104222b6e5314d2889
balarabeel-sudo <balarabeel@gmail.com>
2026-07-16 05:09:54 +0100

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,1 +1,21 @@
 
+function SearchBar() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <input
+        type="text"
+        placeholder="Search farmers, products, companies..."
+        style={{
+          width: "100%",
+          padding: "14px",
+          borderRadius: "12px",
+          border: "1px solid #ddd",
+          fontSize: "16px",
+          outline: "none",
+        }}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
