a766f4885152e4dcd8f00a6c8df194d5797e2f25
balarabeel-sudo <balarabeel@gmail.com>
2026-07-16 04:56:35 +0100

    Add Navbar and welcome message to HomePage

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,1 +1,20 @@
 
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h2>Welcome to FarmLite</h2>
+        <p>
+          The global agriculture social networking platform connecting farmers,
+          buyers, companies, investors and experts.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default HomePage;
