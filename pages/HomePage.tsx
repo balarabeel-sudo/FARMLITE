4eec624aee66d2b0c41b437f6e9491c23690ea88
balarabeel-sudo <balarabeel@gmail.com>
2026-07-17 01:52:14 +0100

    Update HomePage.tsx

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,19 +1,15 @@
-import BottomNavigation from "../components/BottomNavigation";
-import MainLayout from "../components/MainLayout";
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import SearchBar from "../components/SearchBar";
 import QuickActions from "../components/QuickActions";
 
-function HomePage() {
+const HomePage = () => {
   return (
-    <MainLayout>
-      <Navbar />
+    <>
       <HeroSection />
       <SearchBar />
       <QuickActions />
-    </MainLayout>
+    </>
   );
-}
+};
 
 export default HomePage;
